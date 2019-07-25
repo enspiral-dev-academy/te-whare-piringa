b0162a866fa262c2a9876bc6c74f10c29fb7d3ba
Don Smith <don@antiantonym.com>
2019-07-25 16:08:51 +1200

    update environment variables

--- shared/vars.js
+++ shared/vars.js
@@ -1,9 +1,10 @@
 module.exports = {
   nodeEnv: process.env.NODE_ENV,
-  mongoDbUri: process.env.MONGODB_URI,
-  prodMongoDbUri: process.env.PROD_MONGODB_URI,
+  mongoDbUrl: process.env.MONGODB_URI,
+  prodMongoDbUrl: process.env.PROD_MONGODB_URI,
   mailgunApiKey: process.env.MAILGUN_API_KEY,
-  adminUserId: process.env.ADMIN_USER_ID,
-  testUserId: process.env.TEST_USER_ID,
-  callbackUrl: process.env.CALLBACK_URL
+  adminUserEmail: process.env.ADMIN_USER_EMAIL,
+  adminUserPassword: process.env.ADMIN_USER_PASSWORD,
+  testUserEmail: process.env.TEST_USER_EMAIL,
+  testUserPassword: process.env.TEST_USER_PASSWORD
 }
